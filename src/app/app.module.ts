c1c19d79c5933d4c295b238374f052dd4a4a19b0
mingxuan8833 <mingxuan8833@gmail.com>
2023-12-06 05:49:53 +0800

    done fetch api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpHeaders, HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { RegisterpageComponent } from './registerpage/registerpage.component';
+import { TraintestPipe } from './traintest.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    LoginpageComponent,
+    RegisterpageComponent,
+    TraintestPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
